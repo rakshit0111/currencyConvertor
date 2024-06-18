585ef617f4dd21992c5a001f3dbf2e49e3df2856
rakshit0111 <rakshitsingh333@gmail.com>
2024-06-19 00:27:40 +0530

    confirmations

--- currency/src/App.jsx
+++ currency/src/App.jsx
@@ -32,8 +32,13 @@ function App() {
     };
 
     return (
+      <div>
+                         <h1 className='bg-gray-900 text-green-500 text-center text-2xl font-roboto'>Currency Convertor</h1>
         <div className="min-h-screen flex items-center justify-center bg-cover bg-no-repeat" style={{ backgroundImage: `url('https://static.vecteezy.com/system/resources/previews/006/852/804/original/abstract-blue-background-simple-design-for-your-website-free-vector.jpg')` }}>
-            <div className="w-full max-w-md mx-auto bg-white bg-opacity-30 shadow-lg rounded-lg p-5">
+              
+     
+            <div className="w-full max-w-md mx-auto bg-white bg-opacity-30 shadow-lg rounded-lg p-5 font-roboto">
+              
                 <form onSubmit={(e) => { e.preventDefault(); convert(); }}>
                     <div className="mb-4">
                         <InputBox
@@ -45,16 +50,17 @@ function App() {
                             selectCurrency={from}
                         />
                     </div>
-                    <div className="relative mb-4">
+                    <div className="relative mb-4 font-roboto">
                         <button
                             type="button"
-                            className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-1"
+                            className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-gray-900 text-green-500 px-4 py-2  hover:px-6 hover:py-4 transition duration-300 hover:text-xl" 
                             onClick={swap}
+                            font-roboto
                         >
                             Swap
                         </button>
                     </div>
-                    <div className="mb-4">
+                    <div className="mb-4 font-roboto" >
                         <InputBox
                             label="To"
                             amount={convertedAmount}
@@ -64,11 +70,12 @@ function App() {
                             amountDisable
                         />
                     </div>
-                    <button type="submit" className="w-full bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-lg">
+                    <button type="submit" className="w-full  bg-gray-900 text-green-500 hover:text-xl  px-4 py-2 rounded-lg font-roboto">
                         Convert {from.toUpperCase()} to {to.toUpperCase()}
                     </button>
                 </form>
             </div>
+        </div>
         </div>
     );
 }
